c5032ed043582f32676cfed20154a8de13a8e5dd
turkifyy <126617740+turkifyy@users.noreply.github.com>
2025-03-27 17:55:35 +0000

    notification.js

---
+++ notification.js
@@ -0,0 +1,9 @@
+ <script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+<script>
+  window.OneSignalDeferred = window.OneSignalDeferred || [];
+  OneSignalDeferred.push(async function(OneSignal) {
+    await OneSignal.init({
+      appId: "9925d3a5-a4d6-4d93-a93a-072629ac557d",
+    });
+  });
+</script>
